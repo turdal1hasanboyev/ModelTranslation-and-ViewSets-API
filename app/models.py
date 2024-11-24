bd2821b01005ed05a5564cc2be5ef259c0368db7
turdal1hasanboyev <turdalihasanboyev70.com>
2024-11-24 11:18:04 +0500

    'modeltranslation'

--- app/models.py
+++ app/models.py
@@ -1,4 +1,3 @@
 from django.db import models
 
 # Create your models here.
-from rest_framework.viewsets import ModelViewSet
--- config/settings.py
+++ config/settings.py
@@ -23,6 +23,14 @@ INSTALLED_APPS = [
     'app',
 ]
 
+REST_FRAMEWORK = {
+    # Use Django's standard `django.contrib.auth` permissions,
+    # or allow read-only access for unauthenticated users.
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    ]
+}
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
